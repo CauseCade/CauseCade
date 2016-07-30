1468c5cb0d74ddecad1bb125f89614dd22af1055
sandcrawler <nemoandrea@gmail.com>
2016-07-30 23:59:44 +0200

    Minor Layout Edits

--- web/main.dart
+++ web/main.dart
@@ -1,8 +1,50 @@
-// Copyright (c) 2016, <your name>. All rights reserved. Use of this source code
-// is governed by a BSD-style license that can be found in the LICENSE file.
+import 'dart:math' as math;
+import 'package:d3/d3.dart';
 
-import 'dart:html';
+main() {
+  var width = 800, height = 800;
 
-void main() {
-  querySelector('#output').text = 'Your Dart app is running.';
+  var color = new OrdinalScale.category20();
+
+  var force = new Force()
+    ..charge = -120
+    ..linkDistance = 60
+    ..size = [width, height];
+
+  var svg = new Selection("#GraphHolder").append("svg")
+    ..attr["width"] = "$width"
+    ..attr["height"] = "$height";
+
+  json("miserables.json").then((graph) {
+    force
+      ..nodes = graph['nodes']
+      ..links = graph['links']
+      ..start();
+
+    var link =
+        svg.selectAll(".link").data(graph['links']).enter().append("line")
+          ..attr["class"] = "link"
+          ..styleFn["stroke-width"] = (d) => math.sqrt(d['value']);
+
+    var node =
+        svg.selectAll(".node").data(graph['nodes']).enter().append("circle")
+          ..attr["class"] = "node"
+          ..attr["r"] = "5"
+          ..styleFn["fill"] = ((d) => color(d['group']))
+          ..call((_) => force.drag());
+
+    node.append("title")..textFn = (d) => d['name'];
+
+    force.onTick.listen((_) {
+      link
+        ..attrFn["x1"] = ((d) => d['source']['x'])
+        ..attrFn["y1"] = ((d) => d['source']['y'])
+        ..attrFn["x2"] = ((d) => d['target']['x'])
+        ..attrFn["y2"] = ((d) => d['target']['y']);
+
+      node
+        ..attrFn["cx"] = ((d) => d['x'])
+        ..attrFn["cy"] = ((d) => d['y']);
+    });
+  }, onError: (err) => throw err);
 }
