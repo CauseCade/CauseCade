6c974f375455245c6945576b79fb55f25cd60344
sandcrawler <nemoandrea@gmail.com>
2017-12-31 13:50:55 +0100

    Update Welcome Component
    Updated styling and now featuring a news section, whose text can be altered in the CauseCade-lessons repository.

--- lib/welcome_modal_component.dart
+++ lib/welcome_modal_component.dart
@@ -2,6 +2,11 @@ import 'package:angular2/core.dart';
 import 'package:angular_components/angular_components.dart';
 import 'teach_service.dart';
 import 'network_style_service.dart';
+//for fetching the welcome text from URL
+import 'package:markdown/markdown.dart' as md;
+import 'dart:html' as htmlDart;
+
+
 
 @Component(
     selector: 'welcome-modal',
@@ -17,9 +22,23 @@ class WelcomeComponent {
   TeachService teachService;
   NetworkStyleService styleService;
 
+  //for the welcome text from URL
+  final String path = 'https://raw.githubusercontent.com/NemoAndrea/CauseCade-lessons/master/Welcome_Text.md';//welcome text url
+  var htmlFromMarkdown;
+
+
   bool isVisible = true; //show by default;
 
-  WelcomeComponent(this.teachService,this.styleService);
+  WelcomeComponent(this.teachService,this.styleService){
+    htmlDart.HttpRequest req = new htmlDart.HttpRequest();
+    req
+      ..open('GET', path)
+      ..onLoadEnd.listen((e){
+        ///print(req.responseText);
+        htmlFromMarkdown=md.markdownToHtml(req.responseText,inlineSyntaxes: [new md.InlineHtmlSyntax()]);
+      })
+      ..send('');
+  }
 
   //closes the modal
   void enterCauseCade(){
